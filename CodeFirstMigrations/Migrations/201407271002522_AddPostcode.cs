8a8c92ff8b86367722a82c3075999792be4cbceb
John Proctor <aibokicker@yahoo.co.uk>
2014-07-27 11:03:26 +0100

    Run Command in PM to create new migration for new field:
    Add-Migration AddPostcode

---
+++ CodeFirstMigrations/Migrations/201407271002522_AddPostcode.cs
@@ -0,0 +1,18 @@
+namespace CodeFirstMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPostcode : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Addresses", "Postcode", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Addresses", "Postcode");
+        }
+    }
+}
