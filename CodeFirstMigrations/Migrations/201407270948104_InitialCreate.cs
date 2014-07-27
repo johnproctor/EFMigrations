d1f609991712d0f288009219b93602a97a14cbcf
John Proctor <aibokicker@yahoo.co.uk>
2014-07-27 10:59:50 +0100

    Run package manager: Enable-Migrations

---
+++ CodeFirstMigrations/Migrations/201407270948104_InitialCreate.cs
@@ -0,0 +1,28 @@
+namespace CodeFirstMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Addresses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        HouseNumber = c.Int(nullable: false),
+                        Street = c.String(),
+                        City = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Addresses");
+        }
+    }
+}
