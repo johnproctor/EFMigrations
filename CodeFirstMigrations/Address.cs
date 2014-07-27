99b1c429812a60aa80ea6f6a124c800aee8b2c89
John Proctor <aibokicker@yahoo.co.uk>
2014-07-27 10:52:06 +0100

    First commit

---
+++ CodeFirstMigrations/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstMigrations
+{
+    public class Address
+    {
+        public Int32 Id { get; set; }
+        public Int32 HouseNumber { get; set; }
+        public String Street { get; set; }
+        public String City { get; set; }
+    }
+}
---
+++ CodeFirstMigrations/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstMigrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MyContext();
+            context.Addresses.Add(new Address()
+            {
+                HouseNumber = 1,
+                Street = "My Road",
+                City = "My City"
+            });
+
+            context.SaveChanges();
+
+            context.Addresses.ToList().ForEach(x => Console.WriteLine("House on Road: " + x.Street));
+
+            Console.ReadKey();
+        }
+    }
+}
